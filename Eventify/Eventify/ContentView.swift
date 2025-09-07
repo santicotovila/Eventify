e7959754ddf41f7091a36b3708061b8a6d6e121c
santicotovila <santicotovila1996@icloud.com>
2025-09-07 11:00:02 +0200

    Intial commit

---
+++ Eventify/Eventify/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Eventify
+//
+//  Created by Santiago Coto Vila on 7/9/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, Team Eventify!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Eventify/Eventify/EventifyApp.swift
@@ -0,0 +1,17 @@
+//
+//  EventifyApp.swift
+//  Eventify
+//
+//  Created by Santiago Coto Vila on 7/9/25.
+//
+
+import SwiftUI
+
+@main
+struct EventifyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
